e2ed3b1b972c3e24cf09cac7373fd4f748e3b4b2
mohamed-eg <mohammed.akel@pme.suezuni.edu.eg>
2025-06-09 20:39:31 +0300

    VerciFrontlfix

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-const back = import.meta.env.VITE_BACKEND_URL || "https://basic-mern-app-1nt2.vercel.app/";
+const back = import.meta.env.VITE_BACKEND_URL || "https://basic-mern-app-1nt2.vercel.app";
 export const useProductStore = create((set) => ({
 	products: [],
 	setProducts: (products) => set({ products }),
